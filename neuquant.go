0f92a86923dfb4c05a4eb646d3a93ec409de9bd3
Edvin CANDON <edvincandon@gmail.com>
2018-03-22 14:09:41 +0100

    Start BMU finder

--- neuquant.go
+++ neuquant.go
@@ -2,6 +2,7 @@ package goquant
 
 import (
 	"image"
+	"math"
 )
 
 type Pixel struct {
@@ -9,48 +10,14 @@ type Pixel struct {
 }
 
 type Neuron struct {
-	weight     Pixel
+	weights     Pixel
 	bias, freq float64
 }
 
 type SOMNetwork struct {
 	network []Neuron
 	input   []Pixel
-}
-
-func (n *SOMNetwork) nextPoint(nextType string) func() int {
-	const (
-		prime1 = 499
-		prime2 = 491
-		prime3 = 487
-		prime4 = 503
-	)
-
-	var fn func() int
-	switch nextType {
-	case "default":
-		s := len(n.input)
-		var p int
-		switch {
-		case s%prime1 != 0:
-			p = prime1
-		case s%prime2 != 0:
-			p = prime2
-		case s%prime3 != 0:
-			p = prime3
-		default:
-			p = prime4
-		}
-
-		pos := 0
-		fn = func() int {
-			pos += p
-			pos = pos % s
-			return pos
-		}
-	}
-
-	return fn
+	beta, gamma float64
 }
 
 func ExtractPixels(m image.Image) []Pixel {
@@ -72,9 +39,6 @@ func ExtractPixels(m image.Image) []Pixel {
 }
 
 func initNeurons(size int, initType string) []Neuron {
-	// sets up the neurons with the initial weights
-	// default method will use the diagonal (0,0,0,0) - (255,255,255,255)
-	// add more later
 	neurons := make([]Neuron, size)
 	initialFreq := 1.0 / float64(size)
 
@@ -83,7 +47,7 @@ func initNeurons(size int, initType string) []Neuron {
 		for i := 0; i < size; i++ {
 			val := float64(i)
 			neurons[i] = Neuron{
-				weight: Pixel{val, val, val, val},
+				weights: Pixel{val, val, val, val},
 				freq:   initialFreq,
 				bias:   0.0,
 			}
@@ -98,12 +62,82 @@ func NewSOMNetwork(size int, input []Pixel) SOMNetwork {
 	network := SOMNetwork{
 		network: neurons,
 		input:   input,
+		beta: 1.0 / 1024.0,
+		gamma : 1024.0,
 	}
 
 	return network
 }
 
+func (som *SOMNetwork) nextPoint(nextType string) func() int {
+	var fn func() int
+	l := len(som.input)
+
+	const (
+		prime1 = 499
+		prime2 = 491
+		prime3 = 487
+		prime4 = 503
+	)
+
+	switch nextType {
+	case "default":
+		var p int
+		switch {
+		case l%prime1 != 0:
+			p = prime1
+		case l%prime2 != 0:
+			p = prime2
+		case l%prime3 != 0:
+			p = prime3
+		default:
+			p = prime4
+		}
+
+		pos := 0
+		fn = func() int {
+			pos += p
+			pos = pos % l
+			return pos
+		}
+	}
+
+	return fn
+}
+
+func (som *SOMNetwork) findBMU(p Pixel) int {
+	l := len(som.network)
+	bestDist := math.MaxFloat64
+	bestBiasDist := bestDist
+	bestPos := 0
+	bestBiasPos := bestPos
+
+	for i := 0; i < l; i++ {
+			n := som.network[i].weights
+			dist := math.Abs(n.r - p.r) + math.Abs(n.g - p.g) + math.Abs(n.b - p.b) + math.Abs(n.a - p.a)
+			if dist < bestDist {
+				bestDist = dist
+				bestPos = i
+			}
+
+			biasDist := dist - som.network[i].bias
+			if biasDist < bestBiasDist {
+				bestBiasDist = biasDist
+				bestBiasPos = i
+			}
+
+			som.network[i].freq -= som.beta * som.network[i].freq
+			som.network[i].bias += som.beta * som.gamma * som.network[i].freq
+		}
+
+	som.network[bestPos].freq += som.beta
+	som.network[bestPos].bias -= som.beta * som.gamma
+
+	return bestPos
+}
+
 func (som *SOMNetwork) Learn(cycles, samplingFactor int, initialRadius float64) {
+	getNextPoint := som.nextPoint("default")
 	l := len(som.input)
 	pixelsPerCycle := l / (cycles * samplingFactor)
 
@@ -111,6 +145,8 @@ func (som *SOMNetwork) Learn(cycles, samplingFactor int, initialRadius float64) 
 	for c < cycles {
 		i := 0
 		for i < pixelsPerCycle {
+			p := som.input[getNextPoint()]
+			bmu := som.findBMU(p)
 			i++
 		}
 		c++
